dd2ffe1afa5481e0a628dbf07124c053bde701ee
Harsit Raj Pathak <102218175+Yuvrajraj0401@users.noreply.github.com>
2026-02-12 22:29:12 +0530

    mongo update

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use('/api/messages', messageRoutes);
 
 // Connect MongoDB
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
